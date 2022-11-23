d69bb528f7f34de075173a7ddff4e5fc5f8e542b
Sebastian Andres Trigos <112731508+20201025113sebas@users.noreply.github.com>
2022-11-23 16:58:27 -0500

    Add files via upload

---
+++ Puntos.py
@@ -0,0 +1,11 @@
+import math
+
+class Person:
+    def __init__(self,x,y):
+        self.x =x
+        self.y =y
+        
+def Calcular_distancia (p1,p2):
+    
+    return sqrt ((p1.x-p2.x)**2+(p1.y - p2.y)**2)
+
